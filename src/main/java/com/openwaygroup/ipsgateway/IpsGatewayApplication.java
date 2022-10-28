6cdfd3b4f775c610e25989465246543aa5e74a6e
nambb <nambb@openwaygroup.com>
2022-10-28 13:39:54 +0700

    init

---
+++ src/main/java/com/openwaygroup/ipsgateway/IpsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.openwaygroup.ipsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openwaygroup/ipsgateway/IpsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openwaygroup.ipsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
