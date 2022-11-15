f75cac3ed5a5932f01de18fad11f0f3be2c7d69f
VietAnh <vietanhnguyen2510@gmail.com>
2022-11-15 17:26:09 +0700

    card loading

---
+++ src/test/java/com/openwaygroup/ipsgateway/CardLoadingTest.java
@@ -0,0 +1,28 @@
+package com.openwaygroup.ipsgateway;
+
+import com.openwaygroup.ipsgateway.entities.Card;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.web.multipart.MultipartFile;
+
+public class CardLoadingTest {
+
+    @Test
+    public void testCardLoading(){
+        Card card = new Card();
+        card.setCardNumber("450000000000000");
+        card.setTrack1("B4500000000000005^JOHN/SMITH                ^24011010000123000");
+        card.setTrack2("4500000000000005=2401101123400001230");
+        card.setChipData("1249F02060000000042349F03060000000000009F1A02070495058000048000\n" +
+                "5F2A0207049A031501129C01009F37040F010E039F34030203009F2701809\n" +
+                "F360200049F260813005F151F946DAA9F10200FA501A000F8000000000000\n" +
+                "000000000F000102030405060708090A0B0C0D0F820218008407F000000704\n" +
+                "0001");
+        card.setPinBlock("C46E9015945AB4AD");
+        card.setTrack1Structure("jj9i");
+        card.setTrack2Structure("jjj");
+
+
+
+    }
+}
